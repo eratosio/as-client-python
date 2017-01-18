e8cd1b827b5afe11b7db4fa17e3603b4d44b12cd
Mac Coombe <Mac.Coombe@csiro.au>
2017-01-19 09:50:18 +1100

    Added some documentation of new model classes.

--- as_client/model.py
+++ as_client/model.py
@@ -3,32 +3,25 @@ from __future__ import division
 
 import collections, inspect
 
-_UNKNOWN = object()
+_UNKNOWN = object() # Placeholder for indicating an unknown value (since None might be a valid value).
 
-class _PropertyData(object):
-    def __init__(self, property_):
-        self._property = property_
-        
-        self._local_value = self._remote_value = _UNKNOWN
-    
-    @property
-    def local_value(self):
-        if self._local_value is not _UNKNOWN:
-            return self._local_value
-        elif self._remote_value is not _UNKNOWN:
-            return self._remote_value
-        else:
-            return self._property.default
-    
-    @local_value.setter
-    def local_value(self, value):
-        self._local_value = value
-    
-    @property
-    def needs_fetch(self):
-        return self._remote_value is _UNKNOWN
+################################################################################
+# Resource property classes.                                                   #
+################################################################################
 
 class _Property(object):
+    """
+    This class implements a data descriptor for the client's "resource" classes
+    (see _Resource below).
+    
+    Specifically, this descriptor is used to manage the resource's attributes
+    that are included in the JSON payload from the API. It includes
+    functionality for extracting the attribute's value from a JSON document,
+    providing a default value if the attribute is not included in the JSON
+    document, triggering a fetch of the resource if the attribute hasn't been
+    retrieved previously, and tracking changes made to the attribute's value on
+    the client side (where permitted).
+    """
     def __init__(self, json_name, from_json=lambda v:v, default=None, writable=False):
         self.json_name = json_name
         self.from_json = from_json
@@ -70,25 +63,77 @@ class _Property(object):
             self.get_data(instance)._remote_value = self.from_json(json[self.json_name])
 
 class _IdProperty(_Property):
+    """
+    A subclass of the _Property data descriptor.
+    
+    This descriptor only differs from the standard _Property descriptor in that
+    it *doesn't* trigger a fetch of the resource if the value hasn't previously
+    been retrieved - for example, if we don't know the resource's ID, there's no
+    point trying to fetch it since the ID is needed to resolve the resource's
+    URL.
+    """
     def __get__(self, instance, owner):
         return self if instance is None else self.get_data(instance).local_value
 
 class _EmbeddedProperty(_Property):
+    """
+    A subclass of the _Property data descriptor.
+    
+    This descriptor differs from the standard _Property descriptor in that when
+    parsing JSON, it extracts the property value from the JSON object's
+    "_embedded" property (if present) rather than from the JSON object itself.
+    This is used to support parsing of Hypertext Application Language payloads.
+    
+    TODO: update to support arbitrary properties of the JSON object, not just
+    "_embedded"?
+    """
     def _update(self, instance, json):
         json = json.get('_embedded', {})
         
         if self.json_name in json:
             self.get_data(instance)._remote_value = self.from_json(json[self.json_name])
 
+class _PropertyData(object):
+    """
+    A class for storing property values.
+    
+    This class is primarily concerned with tracking both a "local" and a
+    "remote" version of the property's value, to allow changes made locally to
+    be tracked.
+    """
+    def __init__(self, property_):
+        self._property = property_
+        
+        self._local_value = self._remote_value = _UNKNOWN
+    
+    @property
+    def local_value(self):
+        if self._local_value is not _UNKNOWN:
+            return self._local_value
+        elif self._remote_value is not _UNKNOWN:
+            return self._remote_value
+        else:
+            return self._property.default
+    
+    @local_value.setter
+    def local_value(self, value):
+        self._local_value = value
+    
+    @property
+    def needs_fetch(self):
+        return self._remote_value is _UNKNOWN
 
-
-
-
-
-
-
+################################################################################
+# Resource base classes.                                                       #
+################################################################################
 
 class _Resource(object):
+    """
+    Base class for all the client's "resource" classes.
+    
+    Does very little, except for provide an algorithm for updating a resource's
+    property values when given a JSON object describing the resource.
+    """
     def _update(self, client, json):
         self._client = client
         
@@ -98,6 +143,13 @@ class _Resource(object):
         return self
 
 class _ResourceList(collections.Sequence):
+    """
+    A thin wrapper around a list of resource class instances.
+    
+    Given a JSON object representing a HAL-encapsulated list of resources,
+    deserialises the resource instances into a list and extracts the associated
+    paging metadata.
+    """
     def __init__(self, client, type_, json):
         self._type = type_
         
@@ -119,6 +171,10 @@ class _ResourceList(collections.Sequence):
     total_count = property(lambda self: self._total_count)
 
 class _ResourceCollection(collections.Sequence):
+    """
+    Implementation of a sequence of resource instances that transparently
+    handles paging of requests to the API.
+    """
     def __init__(self, client, type_, page_size):
         self._client = client
         self._type = type_
@@ -164,10 +220,14 @@ class _ResourceCollection(collections.Sequence):
     def _is_item_loaded(self, index):
         return (self._items is not None) and (self._items[index] is not _UNKNOWN)
 
-
-
+################################################################################
+# Resource classes.                                                            #
+################################################################################
 
 class BaseImage(_Resource):
+    """
+    Representation of the API's "base image" resource type.
+    """
     _url_path = 'base-images'
     _collection = 'baseImages'
     
@@ -183,11 +243,18 @@ class BaseImage(_Resource):
     tags = _Property('tags', set, set())
 
 class HostEnvironment(object):
+    """
+    Representation of the "host environment" type used in the "base image"
+    type's "hostenvironment" property.
+    """
     def __init__(self, json):
         self.architecture = json.get('architecture', None)
         self.operating_system = json.get('operatingsystem', None)
 
 class Model(_Resource):
+    """
+    Representation of the API's "model" resource type.
+    """
     _url_path = 'models'
     _collection = 'models'
     
@@ -200,6 +267,10 @@ class Model(_Resource):
     ports = _EmbeddedProperty('ports', lambda v: [Port(p) for p in v], [])
 
 class Port(object):
+    """
+    Representation of the "port" type used in the "model" type's "ports"
+    property.
+    """
     def __init__(self, json):
         self.name = json.get('portname', None)
         self.required = json.get('required', False)
@@ -208,6 +279,9 @@ class Port(object):
         self.direction = json.get('direction', None)
 
 class Workflow(_Resource):
+    """
+    Representation of the API's "workflow" resource type.
+    """
     _url_path = 'workflows'
     _collection = 'workflows'
     
@@ -220,6 +294,10 @@ class Workflow(_Resource):
     ports = _EmbeddedProperty('ports', lambda v: [WorkflowPort(p) for p in v], [])
 
 class WorkflowPort(object):
+    """
+    Representation of the "port" type used in the "workflow" type's "ports"
+    property.
+    """
     def __init__(self, json):
         self.name = json.get('portname', None)
         self.required = json.get('required', False)

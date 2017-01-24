6fa577b084169b4df9cbf06cf7d7d9b67c4adf3b
Mac Coombe <Mac.Coombe@csiro.au>
2017-01-24 12:56:14 +1100

    Fixed imports.

--- as_client/__init__.py
+++ as_client/__init__.py
@@ -1,6 +1,6 @@
 
 from version import __version__
 
-from client import Client, Error
-from exceptions import RequestError, ServerError
+from client import Client
+from exceptions import Error, RequestError, ServerError
 from model import BaseImage, Model, Workflow
